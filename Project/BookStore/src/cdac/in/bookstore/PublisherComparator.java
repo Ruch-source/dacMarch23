c60ae34e9c0090f208a02e9d4a16a0fad089d29c
Ruch-source <67817202+Ruch-source@users.noreply.github.com>
2023-04-23 22:09:19 +0530

    Delete Project/BookStore directory

--- Project/BookStore/src/cdac/in/bookstore/PublisherComparator.java
+++
@@ -1,9 +0,0 @@
-package cdac.in.bookstore;
-
-import java.util.Comparator;
-
-public class PublisherComparator implements Comparator<Book>{
-	public int compare(Book b1,Book b2) {
-		return b1.getPublisher().compareTo(b2.getPublisher());
-	}
-}
